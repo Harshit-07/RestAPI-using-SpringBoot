bee8a61ff9fa5a4e849c5677d13aa82f80fa930b
Harshit <harshit1281.cse18@chitkara.edu.in>
2021-11-14 18:50:30 +0530

    Post Method(addTopic())

--- src/main/java/io/springbootstarter/topic/TopicController.java
+++ src/main/java/io/springbootstarter/topic/TopicController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,5 +24,10 @@ public class TopicController {
 	@RequestMapping("/topics/{id}")
 	public Topics getTopics(@PathVariable String id) {
 		return topicService.getTopics(id);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST,value="/topics")
+	public void addTopic(@RequestBody Topics topic) {
+		topicService.addTopic(topic);
 	}
 }
--- src/main/java/io/springbootstarter/topic/TopicService.java
+++ src/main/java/io/springbootstarter/topic/TopicService.java
@@ -1,5 +1,6 @@
 package io.springbootstarter.topic;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -8,11 +9,11 @@ import org.springframework.stereotype.Service;
 @Service
 public class TopicService {
 	
-	private List<Topics> topics = Arrays.asList(
+	private List<Topics> topics = new ArrayList<>(Arrays.asList(
 			new Topics("spring","Spring framework","Spring framework Description"),
 			new Topics("java","Core java","Core java Description"),
 			new Topics("JS","ReactJS","ReactJS framework Description")
-			);
+			));
 	
 	public List<Topics> getAllTopics(){
 		return topics;
@@ -20,6 +21,10 @@ public class TopicService {
 	
 	public Topics getTopics(String id) {
 		 return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topics topic) {
+		topics.add(topic);
 	}
 }
 
